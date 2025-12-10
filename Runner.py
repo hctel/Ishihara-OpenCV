8ea99e2add506da18d9bb5ecb6fd13a18333e035
hctel <22167@ecam.be>
2025-12-10 18:24:35 +0100

    Add superimponed detected frame

--- Runner.py
+++ Runner.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 540)
     font = cv2.FONT_HERSHEY_SIMPLEX
     lastNumber = None
+    previousResultImg = None
     while True:
         ret, frame = cam.read()
         x,y = frame.shape[:2]
@@ -16,7 +17,9 @@ if __name__ == "__main__":
         disp = frame.copy()
         cv2.putText(disp, "Press space to capture, ESC to exit", (0,20), font, 0.7, (0,255,0), 2, cv2.LINE_AA)
         cv2.putText(disp, f"Last number: {lastNumber}", (0,50), font, 0.7, (0,255,0), 2, cv2.LINE_AA)
-        cv2.imshow("", disp)
+        if previousResultImg is not None:
+            disp = cv2.bitwise_or(disp, previousResultImg)
+        cv2.imshow("Camera Display", disp)
         key = cv2.waitKey(1)
         if key == 27:
             break
@@ -27,6 +30,7 @@ if __name__ == "__main__":
                 print(len(circles))
                 img = clustering(img)
                 img = dilate_erode(img, 19)
+                previousResultImg = cv2.cvtColor(img.copy(), cv2.COLOR_GRAY2BGR)
                 cv2.imshow("Detected number", img)
                 number = getNumber(img)
                 lastNumber = number
